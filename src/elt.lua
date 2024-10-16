d076f5f479c97be407130c3dc4d2f7ee51535589
Alejandro Exojo <suy@badopi.org>
2024-10-16 21:52:39 +0200

    Add a Chunks helper "class"

--- src/elt.lua
+++ src/elt.lua
@@ -4,9 +4,37 @@ elt._COPYRIGHT   = 'Copyright (c) 2024 Alejandro Exojo Piqueras'
 elt._DESCRIPTION = 'Template engine in pure Lua'
 elt._VERSION = '0.1.0'
 
+--- Chunks helper class.
+-- Simple helper that can be called to construct a new instance with a certain
+-- metatable, and which has a convenient `append` function.
+local Chunks = {}
+Chunks.CODE = 1
+Chunks.RAW = 2
+Chunks.ESCAPE = 3
+
+function Chunks:append(value, position, kind)
+    assert(self and value and type(value) == 'string')
+    assert((kind and position) or (not kind and not position))
+    if not position then
+        table.insert(self, value)
+    else
+        table.insert(self, {value, position, kind})
+    end
+end
+
+Chunks.__index = Chunks
+
+setmetatable(Chunks, {
+    __call = function()
+        return setmetatable({}, Chunks)
+    end
+})
+
+
 local Generator = {}
 local Parser = {}
 
+elt.Chunks = Chunks
 elt.Generator = Generator
 elt.Parser = Parser
 
--- tests/elt_spec.lua
+++ tests/elt_spec.lua
@@ -11,11 +11,43 @@ describe('Module basics', function()
         assert.is_string(elt._DESCRIPTION)
         assert.is_string(elt._VERSION)
 
+        assert.is_table(elt.Chunks)
         assert.is_table(elt.Generator)
         assert.is_table(elt.Parser)
 
         assert.is_function(elt.compile)
         assert.is_function(elt.render)
     end)
+
+end)
+
+describe('Chunks helper', function()
+    it('is a table with a metatable', function()
+        assert.is_table(elt.Chunks)
+        local mt = getmetatable(elt.Chunks)
+        assert.is_table(mt)
+        assert.is_function(mt.__call)
+    end)
+
+    it('can be created with the call operator', function()
+        local chunks = elt.Chunks()
+        assert.is_table(chunks)
+        assert.is_table(getmetatable(chunks))
+    end)
+
+    it('has an append function', function()
+        local chunks = elt.Chunks()
+        chunks:append('foo')
+        chunks:append('bar')
+        assert.is_equal(#chunks, 2)
+
+        chunks:append('baz', 42, elt.Chunks.CODE)
+        assert.is_equal(#chunks, 3)
+
+        assert.is_string(chunks[1])
+        assert.is_string(chunks[2])
+        assert.is_table(chunks[3])
+    end)
+
 end)
 
