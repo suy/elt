5535b6b868d4532c9c0eb8ac0cacf9b104f7376a
Alejandro Exojo <suy@badopi.org>
2024-10-17 11:48:29 +0200

    Change the style of the OO (new instead of __call)
    I think it makes it a bit easier, and it will be less complicated and
    consistent with the next "classes".

--- src/elt.lua
+++ src/elt.lua
@@ -4,31 +4,32 @@ elt._COPYRIGHT   = 'Copyright (c) 2024 Alejandro Exojo Piqueras'
 elt._DESCRIPTION = 'Template engine in pure Lua'
 elt._VERSION = '0.1.0'
 
+local function shared_new(self)
+    return setmetatable({}, self)
+end
+
 --- Chunks helper class.
 -- Simple helper that can be called to construct a new instance with a certain
 -- metatable, and which has a convenient `append` function.
-local Chunks = {}
-Chunks.CODE = 1
-Chunks.RAW = 2
-Chunks.ESCAPE = 3
+local Chunks = {
+    CODE = 1,
+    RAW = 2,
+    ESCAPE = 3,
 
-function Chunks:append(value, position, kind)
-    assert(self and value and type(value) == 'string')
-    assert((kind and position) or (not kind and not position))
-    if not position then
-        table.insert(self, value)
-    else
-        table.insert(self, {value, position, kind})
-    end
-end
+    new = shared_new,
 
+    append = function(self, value, position, kind)
+        assert(self and value and type(value) == 'string')
+        assert((kind and position) or (not kind and not position))
+        if not position then
+            table.insert(self, value)
+        else
+            table.insert(self, {value, position, kind})
+        end
+    end,
+
+}
 Chunks.__index = Chunks
-
-setmetatable(Chunks, {
-    __call = function()
-        return setmetatable({}, Chunks)
-    end
-})
 
 
 local Generator = {}
